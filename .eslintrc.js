efc88cb095e60ff09b57f3d6eb3e8c28a4eb72c6
kisellsn <sonyakondratskaya@gmail.com>
2024-01-01 21:20:48 +0100

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["standard", "semistandard", "plugin:sonarjs/recommended"],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {},
+  extends: ["plugin:sonarjs/recommended", "semistandard", "standard"],
 };
